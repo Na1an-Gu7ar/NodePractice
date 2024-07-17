9fc22a9e73119a2fbdfc699c0aaa9e74bddc647f
Na1an-Gu7ar <gujarnayan8@gmail.com>
2024-07-18 00:52:50 +0530

    Styling + corrected routes

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const corsConfig = {
-    origin: '*',
-    credential: true,
-    methods: ["GET, POST", "PUT", "DELETE"],
-}
-app.options("", cors(corsConfig))
-app.use(cors(corsConfig))
 require("dotenv").config()
 const path = require('path')
 const productModel = require('./models/product')
@@ -24,14 +16,14 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
-
-app.get("/createUser", (req, res) => {
     res.render("createUser")
 })
 
-app.get("/read", async (req, res) => {
+app.get("/createProduct", isLoggedIn, (req, res) => {
+    res.render("index")
+})
+
+app.get("/read", isLoggedIn, async (req, res) => {
     let allProduct = await productModel.find()
     res.render("read", { product: allProduct })
 })
@@ -54,9 +46,10 @@ app.post("/createUser", (req, res) => {
                 password: hash
             })
 
-            let token = jwt.sign({email: req.body.email, userid: user._id}, "shhhhhh")
+            let token = jwt.sign({ email: req.body.email, userid: user._id }, `${process.env.SECRET_KEY}`)
             res.cookie("token", token)
-            res.send(createdUser)
+            // res.send(createdUser)
+            res.render('read')
         })
     })
 })
@@ -66,35 +59,46 @@ app.get("/login", (req, res) => {
 })
 
 app.post("/login", async (req, res) => {
-    let user = await userModel.findOne({email: req.body.email})
-    if(!user){
-        res.send("Something went wrong")
+    let user = await userModel.findOne({ email: req.body.email })
+    if (!user) {
+        // res.send("Something went wrong")
+        alert("Something Went wrong")
+        // res.flash("success", 'Something went wrong')
     }
-    bcrypt.compare(req.body.password, user.password, function(err, result) {
-        if(result) {
-            let token = jwt.sign({email: user.email}, "shhhhhh")
+    bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result) {
+            let token = jwt.sign({ email: user.email }, `${process.env.SECRET_KEY}`)
             res.cookie("token", token)
-            res.send("Login successfull")
+            res.redirect("/read")
+            // res.send("Login successfull")
+            // res.flash("success", 'Something went wrong')
         }
-        else{
-            res.send("Something is wrong")
+        else {
+            // res.send("Something is wrong")
+            alert("Something Went wrong")
         }
     })
 })
 
 app.get("/logout", (req, res) => {
     res.cookie("token", "")
-    res.redirect("/createUser")
+    res.render("createUser")
+    res.flash("success", 'Logged Out')
 })
 
 function isLoggedIn(req, res, next) {
-    if(req.cookie.token) {
-        res.send("you must be logged in")
-    }
-    else{
-        let data = jwt.verify(req.cookie.token, "shhhhhh")
-        req.user = data
-        next()
+    let token = req.cookies.token
+    if (token) {
+        jwt.verify(token, `${process.env.SECRET_KEY}`, (err, decoded) => {
+            if (err) {
+                res.redirect("/login")
+            }
+            else {
+                next()
+            }
+        })
+    } else{
+        res.redirect("/login")
     }
 }
 
